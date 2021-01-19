6e3f56080b5892b2cde8c83ba28a98d6b278722e
Yannick Biheul <yannickbiheul@outlook.fr>
2021-01-19 18:52:40 +0100

    Ajout de commentaires

--- src/controllers/FrontController.php
+++ src/controllers/FrontController.php
@@ -23,6 +23,7 @@ class FrontController {
 
     private $twig;
     
+    // Constructeur
     public function __construct($twig) {
         // instanciation du service Genre
         $this->genreService = new GenreService();
@@ -32,40 +33,48 @@ class FrontController {
         $this->twig = $twig;
     }
 
+    // Récupérer tous les genres
     public function genres() {
        $genres = $this->genreService->getAllGenres();    
        echo $this->twig->render('genres.html.twig', [ "genres" => $genres ] );
     }
 
+    // Récupérer tous les acteurs
     public function acteurs() {
        $acteurs = $this->actorService->getAllActors();
        echo $this->twig->render('actor.html.twig', ["acteurs" => $acteurs]);   
     }
 
+    // Récupérer tous les réalisateurs
     public function realisateurs() {
        $reals = $this->directorService->getAlldirecteurs();
        echo $this->twig->render('directors.html.twig', ["reals" => $reals]);  
     }
 
+    // Récupérer tous les films
     public function films() {
         $films = $this->movieService->getAllFilms();
         echo $this->twig->render('films.html.twig', ["films" => $films]); 
     }
 
+    // Récupérer un genre par ID
     public function genre($id) {
         $genre = $this->genreService->getById($id);
         echo $this->twig->render('genreseul.html.twig', ["genre" => $genre]);   
     }
 
+    // Récupérer un film par ID
     public function movie($id) {
         $movie = $this->movieService->getById($id);
         echo $this->twig->render('movie.html.twig', ["movie" => $movie ]);   
     }
 
+    // Afficher la page d'accueil
     public function accueil() {
         echo $this->twig->render('accueil.html.twig'); 
     }
 
+    // Afficher le formulaire d'ajout de film
     public function formFilm() {
         echo $this->twig->render('formFilm.html.twig');
     }
